7e464670f8e142e83fcda92fb203e590c1b8b24d
asadrehan19 <asadrehan1996@gmail.com>
2024-04-05 16:57:20 +0500

    my commit

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -32,6 +32,16 @@ function Card() {
   const setNewRating = (rating) =>
     this.props.dispatch(fooActions.setRating(rating));
 
+  // const mymedia = () => {
+  //   const style = {
+  //     "@media(max-width: 320px)": {
+  //       height: "fitContent",
+  //       marginLeft: "40px",
+  //       backgroundColor: "red",
+  //     },
+  //   };
+  // };
+
   return (
     <div className=" flex flex-wrap ">
       {loading ? (
@@ -44,8 +54,9 @@ function Card() {
             {" "}
             <div
               key={i}
-              className="card mt-3 w-60 h-72 ml-3 hover:scale-105 duration-150 rounded-sm cursor-pointer bg-slate-900"
-              style={{ height: "fit-content" }}
+              className="bgCard card mt-3 w-60 h-72 ml-3 hover:scale-105 duration-150 rounded-sm cursor-pointer bg-slate-900"
+              // style={{ height: "fit-content", @mediaScreen and (min-width: 320px) and (max-width: 350px) bg-slate-900}}
+              style={{ height: "fit-content", marginLeft: "48px" }}
             >
               <img
                 src={e.image}
