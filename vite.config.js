c86c2a72b88a1de29e1e1a54ddba49536e5cea3e
asadrehan19 <asadrehan1996@gmail.com>
2024-04-05 18:01:43 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/professormovies/",
+  base: "/movievault/",
   plugins: [react()],
 });
